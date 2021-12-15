a259246a1cc558dfab61d6df4842040f186d16bc
Jerrad Pinion <jpinion20@yahoo.com>
2021-12-14 18:51:12 -0600

    commit tree e8dfbcc8c7272b78be2cc39298e2e7c98b6dd098

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution here!
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+cats.push("Ralph");
+
+cats.unshift("Bob");
+
+cats.pop();
+
+cats.shift();
+
